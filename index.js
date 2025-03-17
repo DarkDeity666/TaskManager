dffec12211e9e13d01cfb1a721dd9a63769eceaf
DrDead0 <112349103+DrDead0@users.noreply.github.com>
2025-03-18 00:48:40 +0530

    Added Edit page, Feature and Edit routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs= require('fs');
+const { error } = require('console');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -18,15 +19,15 @@ app.get('/',(req,res)=>{
 
 app.get('/files/:filename',(req,res)=>{
     fs.readFile(`./files/${req.params.filename}`,"utf-8",(err,filedata)=>{
-        if(err) throw err;
+        if(err) throw error;
         res.render('show',{filename:req.params.filename,filedata:filedata});
     })
-
 })
 
-app.get(`edit/:filename`,(req,res)=>{
-    
+app.get(`/edit/:filename`,(req,res)=>{
+    res.render('edit',{filename:req.params.filename});
 })
+
 app.post('/create',(req,res)=>{
     fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,(err)=>{
         if(err) throw err;
@@ -36,6 +37,12 @@ app.post('/create',(req,res)=>{
         }
         
     })
+})
+
+app.post('/edit',(req,res)=>{
+        fs.rename(`./files/${req.body.previousTitle}`,`./files/${req.body.newTitle}`,(err)=>{
+               res.redirect('/');
+        })
 })
 
 app.listen(3000,()=>{
